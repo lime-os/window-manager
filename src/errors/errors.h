dcd2bd6581e3ba4e57ef8cd02dd57411a15f51a2
demian-gg <hello@demian.gg>
2024-12-22 15:26:35 +0100

    Add Doxygen documentation to errors.h

--- src/errors/errors.h
+++ src/errors/errors.h
@@ -7,4 +7,16 @@ typedef enum {
     ERROR_FATAL     // Log and exit.
 } ErrorSeverity;
 
+/**
+ * X11 error handler callback function, intended to be passed to
+ * `XSetErrorHandler()`.
+ * 
+ * Determines severity of errors and takes appropriate action (E.g silently 
+ * ignore, log and ignore, log and exit).
+ *
+ * @param display The X11 display.
+ * @param error The X11 error event structure.
+ * @return Always returns 0 to indicate to X11 that the error was handled,
+ * and doesn't need to be propagated further.
+ */
 int error_handler(Display *display, XErrorEvent *error);
